71cdfff00706968c64479e4ea52f13617a8acbcc
Finn 'Gallefray' O'leary <GallefrayVBS@aol.com>
2013-10-19 17:44:56 +0100

    Fixed some indentation problems and cleaned up the code :3
    For some reason GH messed up the indentation... :(

--- JamesTheMadman.lua
+++ JamesTheMadman.lua
@@ -5,76 +5,66 @@ socket = require 'socket'
 mainLoop = 0
 
 bot = {}
-bot.server = "irc.afternet.org"
-bot.port = 6667
-bot.nick = "JamesTheMadman"
-bot.username = "James"
-bot.chan = "#ludumdareD&D" -- D&D -- polyhedrondesign
-bot.admin = "classicalduck"
-bot.asleep = false
-bot.manual = false
-bool = false
+bot.server = "irc.afternet.org" -- Some examples include: irc.afternet.org, chat.freenode.org
+bot.port = 6667                 -- The port number of the server to connect to. It's usually 6667
+bot.nick = "JamesTheMadman"     -- The bot's name in the channel
+bot.username = "James"          -- The name it'll show in any WHOIS queries
+bot.chan = "#polyhedrondesign"  -- The channel name. Be sure to put the #
+bot.admin = "Gallefray"         -- Put your nick here
+bot.asleep = false              -- Will the bot respond to commands? 
+bot.manual = false              -- Allows you to send messages as the bot
+bool = false                    
 
 irc = {}
-irc.msg = "" -- most recent msg that's been recieved
-irc.msgLower = ""
-irc.respond = 0
+irc.msg = ""                    -- Stores most recent msg that's been recieved
 
-client, err = socket.tcp() -- creates a socket
+client, err = socket.tcp()      -- Creates a TCP socket in the variable client and if there's an error put it in err
 
-if not client then --If client was not created then
-	error(err) --return the error message
+if not client then              -- If client was not created then
+	error(err)                  -- return the error message
 end
 
-client:settimeout(60) --Set the timeout to 60 seconds
+client:settimeout(60)           -- Set the timeout to 60 seconds
 
 print("connecting...")
 client:connect(bot.server, bot.port)
 print("connected")
+
+handleInput()                   -- Handle any PINGs and such
 handleInput()
 handleInput()
-handleInput()
-client:send("NICK " .. bot.nick .. "\r\n") -- \r\n
+
+client:send("NICK " .. bot.nick .. "\r\n")        -- Send the nickname to the server
 print("NICK " .. bot.nick .. "\r\n")
+
 handleInput()
-client:send("USER " .. "x x x x x" .. "\r\n") -- bot.username .. 
-print("USER " .. "x x x x x" .. "\r\n") -- bot.username .. 
+
+client:send("USER " .. "x x x x x" .. "\r\n")     -- Send the username to the server
+print("USER " .. "x x x x x" .. "\r\n")           
+
 handleInput()
-client:send("JOIN " .. bot.chan .. "\r\n")
-print("JOIN " .. bot.chan .. "\r\n")
-sleep(5)
+
+client:send("JOIN " .. bot.chan .. "\r\n")        -- Join the channel
+print("JOIN " .. bot.chan .. "\r\n") 
+sleep(5)                                          -- Sleep for 5 seconds
 
 while true do
-   --print("In main loop")
-   --BufferControl()
-   if bot.manual == true then
-      print("type input: ")
-      input = io.read()
-      if input ~= "james:auto" then
-         sendMsg(input)
-      elseif input == "james:auto" then
-         bot.manual = false
-      end
-   else
-      handleInput()
-      if bool == false then
-         print("Sending message")
-         sendMsg("Hey guys!")
-         bool = true
-      end
-   end
+	if bot.manual == true then                    -- If the bot is in manual mode then ask for input from the user
+    	print("type input: ")
+    	input = io.read()
+      
+    	if input ~= "james:auto" then             -- if the input isn't james:auto then make the bot say the intput
+        	sendMsg(input)
+         
+      	elseif input == "james:auto" then         -- If the user types james:auto then it'll switch it back into auto
+        	bot.manual = false
+      	end
+   	else
+    	handleInput()
+    	if bool == false then
+        	print("Sending message")
+        	sendMsg("Hey guys!")
+        	bool = true
+      	end
+   	end
 end
-
-
-
--- function BufferControl()
---    irc.msg = client:receive()
---    buffer = buffer .. irc.msg
---    if string.find(buffer, "\n\r") or string.find(buffer, "\r\n") then
---       print(buffer)
---       MSGstring = buffer
---       buffer = ""
---    else
---       print(buffer)
---    end
--- end
